5a74123f610d59361761b359782dea7c5f64f183
Angie <arco527@gmail.com>
2019-10-07 09:42:04 -0500

    Arrays finish

--- ejercicios/02-ejercicios-arrays.js
+++ ejercicios/02-ejercicios-arrays.js
@@ -12,4 +12,43 @@ do {
 
 } while (pedir != "pare");
 numeros.pop()
-console.log(numeros)
+document.write("Estos son los número ingresados: " + "<p>"+numeros+"</p>")
+// numeros.sort(function(a, b){
+//   console.log("esto es a:" + a + "esto es b:" + b)
+//   return a - b
+
+  var busqueda = numeros.findIndex(function(numero){
+    if (numero == 4){
+      console.log("encontrado en este indice " + busqueda)
+    }else{
+      console.log("sigue intentando")
+    }
+  })
+
+  // console.log(busqueda)
+
+
+
+
+
+
+  // var busqueda = prompt("escribe número")
+  // var posicion = numeros.findIndex(numero => numero == busqueda);
+  //
+  // if (posicion && posicion != -1) {
+  //   console.log("encontrado en la posición: " + posicion)
+  // }else{
+  //   console.log("no fue encontrado")
+  // }
+// })
+// console.log(numeros)
+// console.log(numeros.reverse())
+// console.log(numeros.length)
+
+// var busqueda = numeros.some(function(numero){
+//   if (numero == 2){
+//     console.log("hay un número" + 2)
+//   }else{
+//     console.log("sigue intentando")
+//   }
+// })
