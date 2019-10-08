9e4e8010038790a224e2ec08b8fe2a86fa5da84b
Angie <arco527@gmail.com>
2019-10-07 19:55:10 -0500

    Exercises and DOM, BOM etc

---
+++ js/15-DOM.js
@@ -0,0 +1,30 @@
+'use strict'
+
+//DOM Document Object Model
+
+// function cambiaColor(color){
+//   caja.style.background = color
+// }
+//
+// function fontStyle(font){
+//   fuente.style.fontSize = font;
+// }
+//
+//
+//
+// var caja = document.getElementById("micaja")
+// var fuente = document.getElementById("micaja")
+// var caja = document.querySelector("#micaja")
+//
+// caja.innerHTML = "Hola mundo"
+// caja.style.background = "red"
+// caja.style.padding = "20px"
+// caja.style.color = "white"
+// caja.className =  "hola hola2"
+// fuente.style.fontSize = "32px"
+//
+// cambiaColor(caja)
+// fontStyle(fuente)
+
+
+// COMO SELECCIONAR VARIAS ETIQUETAS
