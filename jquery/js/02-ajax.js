895a9453596d300b5dbb37bac1924fff47545b51
Angie <arco527@gmail.com>
2019-10-23 23:06:43 -0500

    Json with jquery

--- jquery/js/02-ajax.js
+++ jquery/js/02-ajax.js
@@ -5,9 +5,33 @@ $('document').ready(function(){
 
   // $('.datos').load('https://reqres.in/')
 
-  //Get y Post
+  //Get
 
-  $('.datos').get('/api/users?page=2')
+  // $.get("https://reqres.in/api/users", {page: 2}, function(response){
+  //   console.log(response)
+  //   response.data.forEach((element, index) => {
+  //     console.log(element)
+  //     $('.datos').append("<p>" + element.first_name + element.last_name + "</p>")
+  //   })
+  // })
 
 
+  //Post
+
+
+  $('#formulario').submit(function(e){
+    e.preventDefault()
+    var user = {
+      name:$('input[name="name"]').val(),
+      age: $('input[name="age"]').val()
+    }
+
+    $.post($(this).attr("action"), user , function(response){
+      console.log(response)
+      // $('.datos').append("<p>" + user.name + " "  + user.age + "</p>")
+    }).done(function(){
+      alert("usuario añadido correctamente")
+    })
+    return false;
+  })
 })
